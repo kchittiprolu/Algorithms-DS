afc358537bf1ec0252a266aa648ee02e4156bd41
karthik chittiprolu <kchittiprolu@gmail.com>
2019-09-10 12:45:45 -0400

    Updated existing algorithms to include how to call the functions. Added MaxBinaryGapInArrays

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Algorithms_DS.Trees;
+using Algorithms_DS.Arrays;
 
 namespace AlgorithmsDS
 {
@@ -7,33 +8,8 @@ namespace AlgorithmsDS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            int[] input = {5,1,2,4,6,3,0};
-            Tree tree1 = new Tree();
-            tree1.ConstructTree(input);
-            tree1.Inorder();
-            tree1.PostOrder();
-            tree1.PreOrder();
-            var root = tree1.GetRoot();
-            Console.WriteLine("Root:{0}",root.data);
-
-            int[] input2 = {1,1,1,1,1,};
-            Tree tree2 = new Tree();
-            tree2.ConstructTree(input2);
-            tree2.PostOrder();
-            var uniq = new UniqValuesTree();
-            Console.WriteLine(uniq.IsUniqvalTree(tree2.GetRoot()));
-
-            Tree tree3 = new Tree();
-            tree3.ConstructTree(new int[] {1,2,3,4,9});
-            tree3.PostOrder();
-
-            var merge = new MergeTwoBinaryTrees();
-            var mergedTree = merge.MergeTrees(tree1.GetRoot(),tree3.GetRoot());
-            Tree tree = new Tree();
-            Console.WriteLine("Merged Tree:");
-            tree.PreOrderTraversal(mergedTree);
-
+            var binGap = new MaxBinaryGapInArray();
+            Console.WriteLine("Max binary gap:{0}",binGap.MaxbinaryGap(1041));
             Console.Read();
         }
     }
